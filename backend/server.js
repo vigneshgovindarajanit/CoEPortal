91b7526b0bd3e13bdee0f91897577ea602a72391
vigneshgovindarajanit <vigneshgovindarajan.it@gmail.com>
2026-04-16 22:07:13 +0530

    updated code for start derver

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,6 @@
 const env = require('./src/config/env')
 const app = require('./src/app')
-const authService = require('./src/modules/auth/auth.service')
 
-authService
-  .ensureAuthReady()
-  .then(() => {
-    app.listen(env.port, () => {
-      console.log(`API listening on port ${env.port}`)
-    })
-  })
-  .catch((err) => {
-    console.error('Failed to initialize auth:', err.message)
-    process.exit(1)
-  })
+app.listen(env.port, () => {
+  console.log(`API listening on port ${env.port}`)
+})
